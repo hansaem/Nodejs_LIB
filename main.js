90ec6f1b42eb788ddb25771cc49074fd3593f467
hansaem <kingne@naver.com>
2018-12-17 15:51:48 +0000

    npm installs package

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+var express = require("express");
+var app = express();
+
+app.listen(3000, function(){
+	console.log("listen");
+});
+
+app.get("/", function(req,res){
+	res.send("helloworld");
+});
