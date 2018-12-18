3e75bf3ab391b3b6e5318678059fbe68bc0ef025
hansaem <kingne@naver.com>
2018-12-18 13:10:27 +0000

    Intro 작업

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
-	console.log("listen");
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/public/CSS'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jQuery/dist/js'));
+app.use('/public', express.static(__dirname + '/public'));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 app.get("/", function(req,res){
-	res.send("helloworld");
+  res.render('index.html');
+});
+app.get("/introduce", function(req,res){
+	res.render("introduce_copy0.html");
+});
+
+app.get("/serviceHelper", function(req,res){
+	res.render("serviceHelper.html");
+});
+app.get("/shiphelper", function(req,res){
+	res.render("shiphelper.html");
 });
